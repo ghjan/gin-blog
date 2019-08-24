c98c79a05f7f1238441908a9b0a7e5d90c3890b0
jian.zhang <cajan2@163.com>
2019-08-24 10:34:22 +0800

    jwt

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/couchbase/go-couchbase v0.0.0-20190808163554-a309852c6932 // indirect
 	github.com/couchbase/gomemcached v0.0.0-20190515232915-c4b4ca0eb21d // indirect
 	github.com/couchbase/goutils v0.0.0-20190315194238-f9d42b11473b // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/edsrzf/mmap-go v1.0.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.4.0
--- routers/router.go
+++ routers/router.go
@@ -1,7 +1,9 @@
 package routers
 
 import (
+	"github.com/ghjan/gin-blog/middleware/jwt"
 	"github.com/ghjan/gin-blog/pkg/setting"
+	"github.com/ghjan/gin-blog/routers/api"
 	v1 "github.com/ghjan/gin-blog/routers/api/v1"
 	"github.com/gin-gonic/gin"
 )
@@ -14,7 +16,10 @@ func InitRouter() *gin.Engine {
 
 	r.Use(gin.Recovery())
 
+	r.GET("/auth", api.GetAuth)
+
 	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
 	{
 		//获取标签列表
 		apiv1.GET("/tags", v1.GetTags)
